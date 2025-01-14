70816a1a53f305ae7aa86b49b32e4c2fdf298d6c
Vageesha G <vageeshag75@gmail.com>
2025-01-14 13:52:49 +0000

    Added click events to all images so that gets applied to the player class div

--- index.js
+++ index.js
@@ -1,73 +1,94 @@
-//let userChoice = prompt("Type your choice: ");
-let choiceList = ["rock", "paper", "scissors"];
-let humanScore = 0, computerScore = 0;
+// //let userChoice = prompt("Type your choice: ");
+// let choiceList = ["rock", "paper", "scissors"];
+// let humanScore = 0, computerScore = 0;
 
-playGame();
+// playGame();
 
 
 
-function getComputerChoice() {
-   let randomIndex = Math.floor(Math.random() * 3);
-   return choiceList[randomIndex];
-}
+// function getComputerChoice() {
+//    let randomIndex = Math.floor(Math.random() * 3);
+//    return choiceList[randomIndex];
+// }
 
-function getHumanChoice() {
-    let typedChoice = prompt("Type your choice: ");
-    return typedChoice.toLowerCase();
-}
+// function getHumanChoice() {
+//     let typedChoice = prompt("Type your choice: ");
+//     return typedChoice.toLowerCase();
+// }
 
-function playRound () {
-    let computerChoice = getComputerChoice();
-    console.log(`computer choice: ${computerChoice}`);
-    let humanChoice = getHumanChoice();
-    if (computerChoice == humanChoice) {
-        console.log("It's a draw");
-        return;
-    }
-    if (computerChoice == "rock") {
+// function playRound () {
+//     let computerChoice = getComputerChoice();
+//     console.log(`computer choice: ${computerChoice}`);
+//     let humanChoice = getHumanChoice();
+//     if (computerChoice == humanChoice) {
+//         console.log("It's a draw");
+//         return;
+//     }
+//     if (computerChoice == "rock") {
 
-        if (humanChoice == "paper") {
-            console.log("You win!!");
-            humanScore++;
-        } else {
-            console.log("You Lose!!");
-            computerScore++;
+//         if (humanChoice == "paper") {
+//             console.log("You win!!");
+//             humanScore++;
+//         } else {
+//             console.log("You Lose!!");
+//             computerScore++;
+//         }
+
+//     } else if (computerChoice == "paper") {
+
+//         if (humanChoice == "scissors") {
+//             console.log("You win!!");
+//             humanScore++;
+//         } else {
+//             console.log("You Lose!!");
+//             computerScore++;
+//         }
+
+//     } else if (computerChoice == "scissors") {
+
+//         if (humanChoice == "paper") {
+//             console.log("You Lose!!");
+//             //
+//             computerScore++;
+
+//         } else {
+//             console.log("You Win!!");
+//             humanScore++;
+//         }
+
+//     }
+// }
+
+// function playGame() {
+//     for (let i = 1; i <= 5; i++) {
+//         playRound();
+//     }
+//     if (humanScore > computerScore) {
+//         console.log("You win the entire game!!!");
+//     } else if (humanScore < computerScore) {
+//         console.log("You Lost the game!!!");
+//     } else {
+//         console.log("It's a draw game!!!");
+//     }
+// }
+
+
+const choices = document.querySelectorAll(".choices img");
+
+choices.forEach(choice => {
+    choice.addEventListener("click", function (e) {
+        let findImage = document.querySelector(".player img");
+        if (findImage != null) {
+            findImage.remove();
         }
+        const playerDiv = document.querySelector(".player");
+        const requiredImageURL = (e.target).getAttribute("src");
+        let requiredImage = document.createElement("img");
+        requiredImage.setAttribute("src", requiredImageURL);
+        playerDiv.insertBefore(requiredImage, playerDiv.children[0]);
+    });
+})
 
-    } else if (computerChoice == "paper") {
-
-        if (humanChoice == "scissors") {
-            console.log("You win!!");
-            humanScore++;
-        } else {
-            console.log("You Lose!!");
-            computerScore++;
-        }
-
-    } else if (computerChoice == "scissors") {
-
-        if (humanChoice == "paper") {
-            console.log("You Lose!!");
-            //
-            computerScore++;
-
-        } else {
-            console.log("You Win!!");
-            humanScore++;
-        }
-
-    }
-}
-
-function playGame() {
-    for (let i = 1; i <= 5; i++) {
-        playRound();
-    }
-    if (humanScore > computerScore) {
-        console.log("You win the entire game!!!");
-    } else if (humanScore < computerScore) {
-        console.log("You Lost the game!!!");
-    } else {
-        console.log("It's a draw game!!!");
-    }
+function addImage() {
+    
 }
